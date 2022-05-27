13672e28f8050b0d374b4fbf507aabb535f1e9e2
NamanPhagiwala <namanphagiwala21@gmail.com>
2022-05-27 13:35:57 +0530

    Changes

--- packages/react-components/src/LinkExternal.tsx
+++ packages/react-components/src/LinkExternal.tsx
@@ -74,8 +74,8 @@ function LinkExternal ({ className = '', data, hash, isLogo, isSidebar, isSmall,
 
   return (
     <div className={`${className} ${isLogo ? 'isText' : 'isLogo'}${isSmall ? ' isSmall' : ''}${isSidebar ? ' isSidebar' : ''}`}>
-      {(isLogo && !isSmall) && <div>{t<string>('View this externally')}</div>}
-      {/* <div className='links'>{links.map((link, index) => <span key={index}>{link}</span>)}</div> */}
+      {/* {(isLogo && !isSmall) && <div>{t<string>('View this externally')}</div>}
+      <div className='links'>{links.map((link, index) => <span key={index}>{link}</span>)}</div> */}
     </div>
   );
 }
